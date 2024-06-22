3559aa289ed7671e21ab2c29f02b0da5b9e66277
PeterEkwere <peterekwere@icloud.com>
2024-06-22 18:00:40 +0100

    updated wallets.html | deposit.html | and added endpoints for both

--- run_test.py
+++ run_test.py
@@ -1,83 +1,10 @@
-from alpaca.data.live.crypto import CryptoDataStream
-from config import API_KEY, API_SECRET
-from alpaca.trading.client import TradingClient
-from alpaca.trading.requests import GetAssetsRequest, MarketOrderRequest
-from alpaca.trading.enums import OrderSide, TimeInForce, AssetClass
-import datetime
-import random
-import time
-import json
-
-#TRADER_API_KEY = 'PK4AZDP245Z3OQ8MMAPB'
-#TRADER_SECRET_KEY = '9D3rGxqT2FZkrR4JKxp7YAJqcp2EvGdcp8XNb6VC'
-
-class Trade:
-    """ THis Class encapsulates all user Trade logic
-    """
-    
-    def __init__(self) -> None:
-        pass
-
-
-    def place_order(self, symbol: str, quantity: int, stop_loss: int, take_profit: int, Condition: str, time_frame: str):
-        """ This  Method handles user trades Buy/Sell
-            Example symbol = "BTC/USD", quantity = 0.023, Condition = client_func["BUY"]
-        """
-        
-        user_trade = {
-            "symbol": symbol,
-            "quantity": quantity,
-            "stop_loss": stop_loss,
-            "take_profit": take_profit,
-            "condition": Condition,
-            "time_frame": time_frame
-        }
-        return user_trade
+import itertools
 
 
 
-#trade = Trade()
+words = ['exact', 'ticket', 'mystery', 'scissors', 'today', 'gift', 'december', 'broken', 'frown', 'walk', 'spare', 'ice']
 
-#market_order = trade.place_order("BTC/USD", 0.4, "BUY")
-#print(market_order)
+permutations = list(itertools.permutations(words))
 
-
-#assets = Trade.assets
-#print("Saving assets")
-#print(f"{assets}")
-#a_dict = {}
-#for asset in assets:
-#    a_dict[f"{asset.id}"] = { asset.symbol: asset.price_increment}
-    
-#json_string = json.dumps()
-
-#with open('assets.json', 'w') as file:
-#    json.dump(a_dict, file, indent=2)
-
-
-
-# keys are required for live data
-#crypto_stream = CryptoDataStream(API_KEY, API_SECRET)
-
-#wss_client = CryptoDataStream(TRADER_API_KEY, TRADER_SECRET_KEY)
-
-# async handler
-#async def quote_data_handler(data):
-    # quote data will arrive here
-#    print(data)
-
-#wss_client.subscribe_quotes(quote_data_handler, "SPY")
-
-#wss_client.run()
-
-#accounts = get_all_accounts()
-#rint(accounts)
-#achrelationship = create_trading_account("Og@gmail.com", "OGT", "08025415081")
-#fund_account(achrelationship)
-#print(f"Account id is {account.id}\n Account name is {account.identity.given_name} \n Account Number is {account.account_number}\n")
-
-
-import random
-
-digits = ''.join([str(random.randint(0, 9)) for _ in range(5)])
-print(digits)
+for p in permutations[:10]:
+    print(p)
